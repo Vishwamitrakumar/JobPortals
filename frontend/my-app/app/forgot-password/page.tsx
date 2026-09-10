7f78a7704f76b23917b5f4c9377e4ddccc98de9a
Vishwamitrakumar <kumarvishwamitra14@gmail.com>
2026-09-10 14:10:11 -0700

    ui change

---
+++ frontend/my-app/app/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import ForgotpasswordPa from "@/components/ForgotpasswordPage";
+
+export default function ForgotpasswordPage() {
+  return (
+    <div className="space-y-6 px-3 sm:space-y-8 sm:px-4 lg:px-6">
+      <ForgotpasswordPa />
+    </div>
+  );
+}
